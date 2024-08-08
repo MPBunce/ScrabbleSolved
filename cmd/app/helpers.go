003699a9d61c9a4ceb39444b4e012384eb90de40
Matthew Bunce <77083766+MPBunce@users.noreply.github.com>
2024-08-08 18:15:40 -0400

    two ways and then 1 and two off

--- cmd/app/helpers.go
+++ cmd/app/helpers.go
@@ -113,6 +113,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 }
 
+func generateCombinations(input string) {
+
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.logger.Fatal(2, trace)
